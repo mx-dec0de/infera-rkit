ebfd458c08b30174a4cad35c0f1f86d0736bca62
dec0de <mx.dec0de@gmail.com>
2024-11-28 09:39:19 +0800

    ini + d4

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import{ useState } from "react";
+import { useState } from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount, usePublicClient, useWalletClient } from "wagmi";
 import { formatUnits, parseUnits } from "ethers";
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['ethers'], // Указываем, что Vite должен оптимизировать ethers
+  },
+  resolve: {
+    alias: {
+      stream: 'stream-browserify', // Если ethers требует stream
+    },
+  },
   build: {
     rollupOptions: {
-      external: ["ethers"],
+      external: ['ethers'], // Указываем ethers как внешнюю зависимость для сборки
     },
   },
 });
